e40ca985465d901febe1e277db25e902bbc8451f
MrDzik <wiktorio04@outlook.com>
2023-06-23 23:42:20 +0200

    feat: added Gate and GateHelper

---
+++ GateCaller/Classes/Gate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GateCaller.Classes
+{
+    internal class Gate
+    {
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public Gate(string name, int phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
---
+++ GateCaller/Helpers/GateHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using GateCaller.Classes;
+
+namespace GateCaller.Helpers
+{
+    internal static class GateHelper
+    {
+        public static List<Gate> Gates = new List<Gate>();
+
+        public static async Task LoadGates()
+        {
+            Gates.Clear();
+            var gates = await SecureStorage.Default.GetAsync("Gates");
+            if (string.IsNullOrEmpty(gates)) return;
+            Gates = JsonSerializer.Deserialize<List<Gate>>(gates);
+        }
+
+        public static async Task UpdateGates()
+        {
+            SecureStorage.Default.Remove("Gates");
+            await SecureStorage.Default.SetAsync("Gates", JsonSerializer.Serialize(Gates));
+        }
+
+        public static void ResetGates()
+        {
+            Gates.Clear();
+            SecureStorage.Default.Remove("Gates");
+        }
+    }
+}
