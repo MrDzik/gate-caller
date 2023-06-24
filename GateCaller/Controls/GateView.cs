b70447c5f1987eb3160fc4050d9d76db67a566a9
MrDzik <wiktorio04@outlook.com>
2023-06-24 21:11:49 +0200

    feat: added MainPage ui and calls are now working

---
+++ GateCaller/Controls/GateView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GateCaller.Controls
+{
+    // This class is now a mirror of SettingsGateView,
+    // but this will change when new features comes in 
+    internal class GateView : ContentView
+    {
+        public static readonly BindableProperty GateNameProperty = BindableProperty.Create(nameof(GateName), typeof(string), typeof(SettingsGateView), string.Empty);
+        public static readonly BindableProperty GatePosProperty = BindableProperty.Create(nameof(GatePos), typeof(int), typeof(SettingsGateView));
+
+        public string GateName
+        {
+            get => (string)GetValue(GateNameProperty);
+            set => SetValue(GateNameProperty, value);
+        }
+        public int GatePos
+        {
+            get => (int)GetValue(GatePosProperty);
+            set => SetValue(GatePosProperty, value);
+        }
+    }
+}
--- GateCaller/Pages/MainPage.xaml.cs
+++ GateCaller/Pages/MainPage.xaml.cs
@@ -1,11 +1,62 @@
-﻿namespace GateCaller;
+﻿using GateCaller.Controls;
+using GateCaller.Helpers;
+
+namespace GateCaller;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
 	{
 		InitializeComponent();
-		
-	}
+    }
+
+    private void MainPage_Reload(object sender, EventArgs e)
+    {
+        Task.Run(LoadData);
+    }
+
+    private async Task LoadData()
+    {
+        Dispatcher.Dispatch(() =>
+        {
+            LoadingLabel.IsVisible = false;
+        });
+        await GateHelper.LoadGates();
+        var count = GateHelper.Gates.Count;
+        Dispatcher.Dispatch(() =>
+        {
+            StackLayout.Children.Clear();
+            if (count == 0)
+            {
+                StackLayout.Children.Add(new Label()
+                {
+                    Text = "Nie posiadasz jeszcze żadnych bram, może jakąś dodasz?",
+                    HorizontalTextAlignment = TextAlignment.Center
+                });
+            }
+            else
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    StackLayout.Children.Add(new GateView()
+                    {
+                        GateName = GateHelper.Gates[i].Name,
+                        GatePos = i,
+                        ControlTemplate = (ControlTemplate)Resources["GateViewControlTemplate"]
+                    });
+                }
+            }
+            LoadingLabel.IsVisible = false;
+        });
+    }
+
+    private void OpenButton_OnClicked(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        var gate = (GateView)button.BindingContext;
+#if ANDROID
+        MainActivity.CallPhone(GateHelper.Gates[gate.GatePos].PhoneNumber);
+#endif
+    }
 }
 
